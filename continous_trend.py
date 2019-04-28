c058127662d059ebf5088a94aa53e87c8bd6ffb8
nuxie <zkalinowska96@icloud.com>
2019-04-28 22:51:23 +0200

    add the proper docstrings

--- continous_trend.py
+++ continous_trend.py
@@ -2,10 +2,22 @@ import numpy as np
 
 
 def continuity_matrix(segments_num, segments_size):
-    # returns E matrix responsible for continuity used on lhs of the equation
-    # continuity condition: Ep = d checked in (segments_num-1) "middle points"
-    # number of rows - number of "middle points" where continuity is checked
-    # number of columns - 4 for one row, each additional row adds 2 columns
+    """ Returns E matrix derived from continuity condition.
+
+    E matrix is used on the left-hand side of the set of equations.
+    It is represented as a two dimensional list.
+    Number of rows equals number of segments - 1.
+    For each row there are 4 non-zero elements.
+
+    :param segments_num: number of segments in the segmented series
+    :type segments_num: int
+    :param segments_size: size of one segment in the segmented series
+    :type segments_size: int
+
+    :return: E matrix in a form of list
+    :rtype: 2dim list, shape=(segments_num-1, 4+(segments_num-2)*2)
+    """
+
     E = np.zeros(shape=(segments_num-1, 4+(segments_num-2)*2))
     for i in range(0, segments_num-1):
         tmp = (i+1)*segments_size + 0.5
@@ -17,8 +29,24 @@ def continuity_matrix(segments_num, segments_size):
 
 
 def Q_matrix(segments_num, segments_size):
-    # returns matrix partially responsible for DFA method conditions
-    # matrix Q - used on lhs of the equation, trend line values
+    """ Returns Q matrix derived from DFA method conditions.
+
+    Q matrix is used on the left-hand side of the set of equations.
+    It is derived from the difference between trend line and actual
+    time series values that we are trying to minimise.
+    It is represented as a three dimensional list.
+    Number of "Q's" equals number of segments.
+    Each "Q" is a list of 2x2 dimensions.
+
+    :param segments_num: number of segments in the segmented series
+    :type segments_num: int
+    :param segments_size: size of one segment in the segmented series
+    :type segments_size: int
+
+    :return: Q matrix in a form of a list
+    :rtype: 3dim list, shape=(segments_num, 2, 2)
+    """
+
     Q = np.zeros(shape=(segments_num, 2, 2))
     for i in range(0, segments_num):
         sum_i_sq = 0
@@ -34,8 +62,28 @@ def Q_matrix(segments_num, segments_size):
 
 
 def equation_matrices(series, segments_num, Q, E):
-    # matrix M - used on lhs of the equation, merged Q and E matrices
-    # matrix cd - used on rhs of the equation, time series values
+    """ Returns M matrix (merged Q and E) and cd matrix used on the
+    right-hand side of the set of equations.
+
+    M matrix is the leftmost matrix from the set of the equations.
+    It consists of E and Q matrices that are properly positioned.
+    The right-hand side of the equation is the cd matrix.
+
+    :param series: time series in segmented or normal form
+    :type series: list
+    :param segments_num: number of segments in the segmented series
+    :type segments_num: int
+    :param Q: Q matrix derived from DFA method conditions
+    :type Q: 3dim list, shape=(segments_num, 2, 2)
+    :param E: E matrix derived from continuity condition
+    :type E: 2dim list, shape=(segments_num-1, 4+(segments_num-2)*2)
+
+    :return: M and cd, two matrices in the form of lists needed to solve
+            the set of equations
+    :rtype: 2dim list shape=(matrix_size, matrix_size),
+            1dim list shape=matrix_size
+    """
+
     segments_size = len(series[0])
     matrix_size = 2*segments_num + (segments_num-1)
     cd = np.zeros(shape=matrix_size)
@@ -62,6 +110,21 @@ def equation_matrices(series, segments_num, Q, E):
 
 
 def calculate_trend(segmented_series):
+    """ Returns parameters of the calculated continuous linear trend.
+
+    It solves the set of the equations needed to fit a trend line.
+    It returns calculated linear function parameters in a form of a list.
+    The linear trend is meant to be continuous but it also has to have
+    the minimal difference between it and actual time series values.
+    The list is returned without the redundant lambda values.
+
+    :param segmented_series: series divided into segments
+    :type segmented_series: 2dim list
+
+    :return: parameters of the trend line
+    :rtype: list, shape=segments_num
+    """
+
     segments_num = len(segmented_series)
     segments_size = len(segmented_series[0])
     E = continuity_matrix(segments_num, segments_size)
@@ -74,6 +137,19 @@ def calculate_trend(segmented_series):
 
 
 def continuity_test(parameters, segment_len, segments_num):
+    """ Checks whether the fitted line is continuous for every point.
+
+    :param parameters: parameters of the fitted trend lines
+    :type parameters: list, shape=segments_num
+    :param segment_len: size of one segment in the segmented series
+    :type segment_len: int
+    :param segments_num: number of segments in the segmented series
+    :type segments_num: int
+
+    :return: value indicating whether continuity condition is satisfied
+    :rtype: boolean
+    """
+
     testing_points = np.zeros(segments_num-1)
     for i in range(0, segments_num-1):
         testing_points[i] = (i+1)*segment_len + 0.5
