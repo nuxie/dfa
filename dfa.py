9fa2e01d783d8080b405d0cfb8543744eb5b99ba
nuxie <zkalinowska96@icloud.com>
2019-04-28 23:19:50 +0200

    add the proper docstrings

--- dfa.py
+++ dfa.py
@@ -5,6 +5,15 @@ import matplotlib.pyplot as plt
 
 
 def random_walk(series, length):
+    """ Converts the series into a random walk using cumulative sum.
+
+    :param series: time series to be converted
+    :type series: np.array
+
+    :param length: length of the resulting random walk
+    :type length: int
+    """
+
     mean = np.mean(series)
     previous = series[0] - mean
     for i in range(0, length):
@@ -14,6 +23,20 @@ def random_walk(series, length):
 
 
 def fluctuation(segmented_series, parameters):
+    """ Returns the fluctuation value for the segmented series.
+
+    Fluctuation - root-mean-square deviation from the fitted trend.
+
+    :param segmented_series: series divided into segments
+    :type segmented_series: np.array
+    :param parameters: parameters of the fitted trend lines,
+                       shape=len(segmented_series)=number of segments
+    :type parameters: np.array
+
+    :return: fluctuation value for the segmented series
+    :rtype: float
+    """
+
     local_trend = np.zeros(shape=np.shape(segmented_series))
     segment_size = len(segmented_series[0])
     for i in range(len(segmented_series)):
@@ -28,8 +51,20 @@ def fluctuation(segmented_series, parameters):
 
 
 def exponent(F, n, plot):
-    # fits a line into log-log graph of fluctuations against segments sizes and returns its slope
-    # plots a graph if requested
+    """ Fits a line into log-log graph of fluctuations against segments
+        sizes and returns its parameters - slope and intercept.
+
+    :param F: fluctuations calculated for segmented series of each size
+    :type F: np.array
+    :param n: segments sizes corresponding to F values
+    :type n: np.array
+    :param plot: indicates if the plot of the fitted line should be shown
+    :type plot: int
+
+    :return: parameters of the line fitted into the log-log graph
+    :rtype: list
+    """
+
     A = np.vstack([np.log(n), np.ones(len(n))]).T
     res = np.linalg.lstsq(A, np.log(F), rcond=None)[0]
     if plot:
@@ -42,6 +77,19 @@ def exponent(F, n, plot):
 
 
 def dfa(series, plot=0):
+    """ Returns the slope of the line fitted into log-log graph of
+        fluctuations against segments sizes, which is the DFA exponent.
+
+    :param series: time series for which to calculate the DFA exponent
+    :type series: np.array
+    :param plot: optional, defaults to 0
+                 indicates if the plot of the fitted line should be shown
+    :type plot: int
+
+    :return: slope of the line fitted into log-log graph
+    :rtype: float
+    """
+
     data_length = len(series)
     data = list(random_walk(series, data_length))
     max_exponent = np.log2(data_length)
@@ -62,4 +110,3 @@ ts = np.array(ts.values.flatten())
 # ts = np.random.standard_normal(size=8192)
 
 print("DFA exponent for given time series with continous trend lines equals:", dfa(ts, plot=1))
-
